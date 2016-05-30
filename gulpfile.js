ad23400402a26bb452101bd7d9d308962a1dbd49
sloong <ifengkou@hotmail.com>
2016-05-30 17:52:30 +0800

    修复jshint() 写法异常

--- gulpfile.js
+++ gulpfile.js
@@ -8,69 +8,69 @@ let gulp = require('gulp');
 let webpack = require('webpack')
 
 let gutil = require('gulp-util')
-let webpackConf = require('./webpack-dev.config')
 //let webpackConf = require('./webpack-dev.config')
+let webpackConf = require('./webpack.config')
 
 let src = process.cwd() + '/src'
 let assets = process.cwd() + '/assets'
 
 //js check
-gulp.task('hint',() =>{
+gulp.task('hint', () => {
     let jshint = require('gulp-jshint')
     let stylish = require('jshint-stylish')
-    //js 目录下除了lib外其他所有js 验证
+
     return gulp.src([
-        '!'+src+'/js/lib/**/*.js',
-        src+'/js/**/*.js'
+        '!' + src + '/js/lib/**/*.js',
+        src + '/js/**/*.js'
     ])
-    .pipe(jshint)
-    .pipe(jshint.reporter(stylish));
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish));
 })
 
 //clean assets
-gulp.task('clean',['hint'],()=>{
+gulp.task('clean', ['hint'], ()=> {
     let clean = require('gulp-clean')
 
-    return gulp.src(assets,{read:true}).pipe(clean())
+    return gulp.src(assets, {read: true}).pipe(clean())
 })
 
 //run webpack page
-gulp.task('pack',['clean'],(done)=>{
-    webpack(webpackConf,(error,stats) =>{
-        if(err) throw new gutil.PluginError('webpack',err)
-        gutil.log(['webpack'],stas.toString({colors:true}))
-        done()
-    })
-})
+/*gulp.task('pack',['clean'],(done)=>{
+ webpack(webpackConf,(error,stats) =>{
+ if(err) throw new gutil.PluginError('webpack',err)
+ gutil.log(['webpack'],stas.toString({colors:true}))
+ done()
+ })
+ })
 
-//default task
-gulp.task('default',['pack'])
+ //default task
+ gulp.task('default',['pack'])*/
 
 //gulp-min 和 html-loader 冲突
 /*gulp.task('default', ['pack'], () => {
-    let replace = require('gulp-replace')
-    let htmlmin = require('gulp-htmlmin')
+ let replace = require('gulp-replace')
+ let htmlmin = require('gulp-htmlmin')
 
-    return gulp
-        .src(assets + '/!*.html')
-        // @see https://github.com/kangax/html-minifier
-        .pipe(htmlmin({
-            collapseWhitespace: true,
-            removeComments: true
-        }))
-        .pipe(gulp.dest(assets))
-})*/
+ return gulp
+ .src(assets + '/!*.html')
+ // @see https://github.com/kangax/html-minifier
+ .pipe(htmlmin({
+ collapseWhitespace: true,
+ removeComments: true
+ }))
+ .pipe(gulp.dest(assets))
+ })*/
 
 //deploy assets to remote server
-gulp.task('deploy',()=>{
+gulp.task('deploy', ()=> {
     let sftp = require('gulp-sftp')
 
-    return gulp.src(assets+'/**')
+    return gulp.src(assets + '/**')
         .pipe(sftp({
-            host:'[remote server ip]',
+            host: '[remote server ip]',
             remotePath: '/www/app/',
-            user:'foo',
-            pass:'bar'
+            user: 'foo',
+            pass: 'bar'
         }))
 })
 
