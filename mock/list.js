f65d5a1302c46b19df416d4e54370fd7dedb3d5f
sloong <ifengkou@hotmail.com>
2016-05-30 13:59:38 +0800

    功能测试

---
+++ mock/list.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Administrator on 2016/5/30.
+ */
+'use strict';
+
+var res = [
+    {
+        id: 1,
+        title: 'foo',
+        content: 'content foo'
+    },
+    {
+        id: 2,
+        title: 'title bar',
+        content: 'content bar'
+    }
+];
+
+module.exports = res;
--- src/js/pageA.js
+++ src/js/pageA.js
@@ -1,3 +1,39 @@
 /**
  * Created by Administrator on 2016/5/27.
  */
+'use strict';
+
+console.info('require page a');
+
+require('commonCss');
+require('../css/a.css');
+
+require('zepto');
+require('avalon');
+
+var _ = require('lodash');
+
+var logoImg = require('webpackLogo');
+var $logo = $('<img />').attr('src', logoImg);
+
+$('#logo').html($logo);
+
+require.ensure([],function(){
+   var ajax = require('./utils/ajax');
+    var t = _.now();
+
+    ajax.request({
+        url: '/api/list',
+        data: {
+            offset: 0,
+            limit: 5
+        }
+    }).done(function(data) {
+        var template = require('../tmpl/list.tpl');
+        var html = template({list: data || []});
+
+        console.info('ajax took %d ms.', _.now() - t);
+
+        $('#list').html(html);
+    });
+});
